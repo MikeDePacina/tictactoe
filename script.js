e4e548b0f65e8ec41644d365b504b881eda2d5c7
Mike De Pacina <mikedpacina@gmail.com>
2021-10-04 20:23:46 -0700

    functional

--- script.js
+++ script.js
@@ -1,43 +1,122 @@
-const tiles = document.getElementsByTagName('button')
-const resetButton = document.getElementById('reset')
-console.log(tiles)
+const tiles = document.querySelectorAll('.cell')
+const resetButton = document.querySelector('#reset')
+const xPlayer = 'x'
+const oPlayer = 'o'
+const resultText = document.querySelector('.results')
+let currentPlayer = xPlayer
 
-class tictactoe{
 
-    gameBoard(){
-        let board = [[['', '', '']],
-                    [['','','']],
-                    [['','','']]]
+let board = ['','','','','','','','',''] 
+let counter = 0
 
-    }    
 
-    reset(){
-        tiles.forEach(button =>{
-            button.classList.toggle("x")
-        })
+const verticalWin = ()=> {
+    if(board[0] == xPlayer || board[0] == oPlayer){
+        if(board[0] == board[3] && board[3] == board[6])
+        return true
     }
-   
-    updateClass(){
-        let counter = 0
-        if(counter % 2 == 0)
-            button.classList.add = "x"
-        else 
-            button.classList.add = "o"
-        counter++
-
+    if(board[1] == xPlayer || board[1] == oPlayer){
+        if(board[1] == board[4] && board[4] == board[7])
+        return true
     }
+    if( board[2] == xPlayer || board[2] == oPlayer){
+          if(board[2] == board[5] && board[5] == board[8])
+        return true
+    }
+    return false
 }
 
-const game = new tictactoe()
+
+
+const horizontalWin = ()=>{
+    if(board[0] == xPlayer || board[0] == oPlayer){
+        if(board[0] == board[1] && board[1] == board[2])
+        return true
+    }
+    if(board[3] == xPlayer || board[3] == oPlayer){
+        if(board[3] == board[4] && board[4] == board[5])
+        return true
+    }
+    if( board[6] == xPlayer || board[6] == oPlayer){
+          if(board[6] == board[7] && board[7] == board[8])
+        return true
+    }
+    return false
+}
+
+
+
+
+const diagonalWin = () =>{
+    if(board[0] == xPlayer || board[0] == oPlayer)
+        if(board[0] == board[4] && board[4] == board[8])
+            return true
+    if(board[2] == xPlayer || board[2] == oPlayer)
+        if(board[2] == board[4] && board[4] == board[6])
+            return true
+    return false
+}
+
+
+
+const checkWin = ()=>{
+    if(diagonalWin() == true || horizontalWin() == true || verticalWin() == true)
+        return true
+    return false
+}
+
+
+
+const checkDraw = () =>{
+    if(board.includes(''))
+        return false
+    return true
+}
+//|| horizontalWin() === true || diagonalWin() === true) */
+            
+const tileClicked = (e)=>{
+    const index = e.target.getAttribute('data-index')
+        if(board[index] == ''){
+            board[index] = currentPlayer
+            e.target.innerText = currentPlayer
+
+            if(checkWin() == true)
+                resultText.innerText = `${currentPlayer} won`
+
+            if(checkDraw() == true)
+            resultText.innerText = 'Draw'
+         }
+
+        
+        
+  
+        
+    
+    currentPlayer = (currentPlayer == xPlayer ? oPlayer : xPlayer)
+    
+   
+}
+
+
 
 
 tiles.forEach(button =>{
-    button.addEventListener('click', ()=>{
-        game.updateClass()
+   button.addEventListener('click', tileClicked)
+})
+
+
+
+resetButton.addEventListener('click', ()=>{
+    tiles.forEach(button =>{
+        button.innerText = ''
     })
+    for(let i = 0; i < board.length; i++)
+        board[i] = ''
+    
+    
+    resultText.innerHTML = ''
 })
 
-resetButton.addEventListener('click',()=>{
-    game.reset()
 
-})
+
+
